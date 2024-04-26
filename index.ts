42937651fc5f75011e8e461ce36cbbe8e4b54f4d
Tayyaba <tayyabasajid.ai@gmail.com>
2024-04-27 01:25:00 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const randomNumber = Math.floor(Math.random() * 8 + 1);
+
+const input = await inquirer.prompt([
+    {message: "Welcome to The Number Guessing Game! Press Enter to start.",
+     type: "enter key",
+     name: "enterkey"
+    },
+    {message: "Guess a number from 1-8\n", 
+     type: "number",
+     name: "userGuessedNumber",
+    },
+])
+
+if (input.userGuessedNumber === randomNumber){
+    console.log("Congratulations!You have guessed the right number.");
+}
+else (console.log("Oops! You've lost."));
